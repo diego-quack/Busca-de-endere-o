ae7a611db61d4ac242602f97e01438bc0d1c667c
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-07-12 20:45:48 -0300

    Testes e alguns ajustes no script

---
+++ js/js-cep.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+
+    //Faz a instancia do modal:
+    var modal = new bootstrap.Modal("#JanelaEndereco");
+    
+    $("#btn").on("click", function() {
+
+            var cep = $("#cep").val().replace(/\D/g, '');
+    
+            //Verifica se campo cep possui valor informado.
+            if (cep != "") {
+    
+                //Expressão regular para validar o CEP:
+                var validacep = /^[0-9]{8}$/;
+    
+                //Valida o formato do CEP:
+                if(validacep.test(cep)) {
+                    
+                    $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+    
+                        if (!("erro" in dados)) {
+                            $("#rua").val(dados.logradouro);
+                            $("#bairro").val(dados.bairro);
+                            $("#cidade").val(dados.localidade);
+                            $("#uf").val(dados.uf);
+                            modal.show();
+                        } 
+                        else {
+                            //CEP pesquisado não foi encontrado.
+                            alert("CEP não encontrado.");
+                        }
+                    });
+                }
+                else {
+                    //cep é inválido.
+                    alert("Formato de CEP inválido.");
+                }
+            }
+            else {
+                alert("Digite um CEP válido!");
+            }
+    });
+});
+
+
